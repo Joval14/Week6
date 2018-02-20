3ede28a51974b1e307eb6305f8e0245d7257605b
Joval14 <jotaval@outlook.com>
2018-02-20 21:58:14 +0000

    Week6

---
+++ README.sh
@@ -0,0 +1,19 @@
+# Week6
+#A few basic commands of the chapter 6 of the Linux book
+!#/bin/bash
+echo "List of Processes"
+echo
+ps
+
+echo
+man ps
+
+echo "The system is going offline for maintenance in 5 minutes"
+echo
+sudo shutdown -h +5
+
+echo "save all your documents now"
+
+echo "type shutdown -c to cancel the operation"
+
+#Thanks
